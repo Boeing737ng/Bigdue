a62c4216f25be3bbe743f1a3f5ccccec67d099d3
seong954t <seomtae@naver.com>
2018-01-11 22:49:37 -0500

    modify connect_socket function and read_packet function

--- app/read_packet_refactoring.py
+++ app/read_packet_refactoring.py
@@ -7,33 +7,31 @@ import struct
 import ipaddress
 import csv
 from collections import deque
-if sys.platform == 'darwin':
-    import pcapy
+import pcap
 
 CONST_MAX_LEN = 10
 
+# socket을 통해 packet capture (window용) 
+# 추후 pcap 사용시 제거
 def connect_socket():
     """
     connect socket and bind
     """
-    if sys.platform == 'win32':
-        # TODO(LuHa): create socket and bind it!
-        # Get host
-        host = socket.gethostbyname(socket.gethostname())
-        # print('IP: {}'.format(host))
+       
+    # TODO(LuHa): create socket and bind it!
+    # Get host
+    host = socket.gethostbyname(socket.gethostname())
+    # print('IP: {}'.format(host))
+        
+    # Create a raw socket and bind it
+    read_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW)
+    read_socket.bind((host, 0))
 
-        # Create a raw socket and bind it
-        read_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW)
-        read_socket.bind((host, 0))
-
-        # Include IP headers
-        read_socket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-        # Enable promiscuous mode
-        read_socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
-        pass
-    else:
-        return
+    # Include IP headers
+    read_socket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    # Enable promiscuous mode
+    read_socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    
     return read_socket
 
 def retrieve_data(data):
@@ -94,23 +92,37 @@ def export_csv_file(file_name, data):
 
 def read_packet():
     packet_deque = deque(maxlen=CONST_MAX_LEN)
-    try:
-        csv_counter = 0
-        while True:
-            read_whole_packet = connect_socket().recv(1500)
-            retrieved_data=retrieve_data(read_whole_packet)
-            if not (None in retrieved_data):
-                store_packet_in_buffer(packet_deque, retrieved_data)
-            if len(packet_deque) == CONST_MAX_LEN:
-                csv_counter = csv_counter + 1
-                export_csv_file("save_" + str(csv_counter) + ".csv", packet_deque)
-                flush_packets_in_buffer(packet_deque)
-            # print(retrieve_data(read_whole_packet))
-            # break
+    if sys.platform == "win32":
+        try:
+            csv_counter = 0
+            read_whole_packet = connect_socket()
+            while True:
+                read_whole_packet = read_whole_packet.recv(1500)
+                retrieved_data=retrieve_data(read_whole_packet)
+                if not (None in retrieved_data):
+                    store_packet_in_buffer(packet_deque, retrieved_data)
+                if len(packet_deque) == CONST_MAX_LEN:
+                    csv_counter = csv_counter + 1
+                    export_csv_file("save_" + str(csv_counter) + ".csv", packet_deque)
+                    flush_packets_in_buffer(packet_deque)
+                # print(retrieve_data(read_whole_packet))
+                # break
 
-    except socket.timeout:
-        print('!Message: Socket time-out')
-        return
+        except socket.timeout:
+            print('!Message: Socket time-out')
+            return
+    else:
+        read_whole_packet = pcap.pcap(name=None, promisc=True, immediate=True, timeout_ms=50)
+        for ts, pkt in read_whole_packet:
+            retrieved_data = retrieve_data(pkt)
+            if not(None in retrieved_data):
+                packet_deque.append(retrieved_data)
+                print(retrieved_data)
+
+            if len(packet_deque) == CONST_MAX_LEN:
+                print(packet_deque)
+                # make_csv_file("save_"+str(i)+".csv", packet_deque)
+                packet_deque.clear()
 
 def store_packet_in_buffer(packet_deque, retrieved_data):
     packet_deque.append(retrieved_data)
