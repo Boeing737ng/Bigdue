441d866e637f53de1ea3db15d7a81397c90639e6
munhyunsu <munhyunsu@gmail.com>
2018-01-17 15:48:30 -0500

    test unit

--- app/Practice/Packet/initiator.py
+++ app/Practice/Packet/initiator.py
@@ -15,8 +15,8 @@ def get_packet_from_bytes(data):
     return:
         packet
     """
-    packet_types = {'l2': None,
-                    'l3': None}
+    packet_types = {'l2': packet.L2UNKNOWN,
+                    'l3': packet.L3UNKNOWN}
     if len(data) < 1:
         raise Exception('The size of data to using create packet below 0')
 
@@ -26,7 +26,19 @@ def get_packet_from_bytes(data):
         if l2_type == packet.L2IP:
             packet_types['l2'] = packet.L2IP
         else:
-            packet_types['l2'] = packet.L2UNKNOWN
+            return packet_types
+        cursor = data[14:]
+
+    if len(cursor) < 20:
+        return packet_types
+
+    l3_protocol = struct.unpack('!BBHHHBBH4s4s', cursor[0:20])[6]
+    if l3_protocol == packet.L3TCP:
+        packet_types['l3'] = packet.L3TCP
+    elif l3_protocol == packet.L3UDP:
+        packet_types['l3'] = packet.L3UDP
+    else:
+        return packet_types
 
     return packet_types
 
@@ -57,3 +69,79 @@ class PacketGetterTextCase(unittest.TestCase):
                             + '1234'))
         self.assertNotEqual(get_packet_from_bytes(data)['l2'], 
                             packet.L2IP)
+
+    def test_l3unknown(self):
+        data = bytes.fromhex(('ffffffffffff' 
+                            + 'ffffffffffff' 
+                            + '0800'
+                            + 'ffffffff'
+                            + 'ffffffff'
+                            + 'ff00ffff'
+                            + 'ffffffff'
+                            + 'ffffffff'))
+        self.assertEqual(get_packet_from_bytes(data)['l3'],
+                         packet.L3UNKNOWN)
+        data = bytes.fromhex(('ffffffffffff' 
+                            + 'ffffffffffff' 
+                            + '0800'
+                            + 'ffffffff'
+                            + 'ffffffff'
+                            + 'ff06ffff'
+                            + 'ffffffff'
+                            + 'ffffffff'))
+        self.assertNotEqual(get_packet_from_bytes(data)['l3'], 
+                            packet.L3UNKNOWN)
+        data = bytes.fromhex(('ffffffffffff' 
+                            + 'ffffffffffff' 
+                            + '0800'
+                            + 'ffffffff'
+                            + 'ffffffff'
+                            + 'ff11ffff'
+                            + 'ffffffff'
+                            + 'ffffffff'))
+        self.assertNotEqual(get_packet_from_bytes(data)['l3'], 
+                            packet.L3UNKNOWN)
+        
+    def test_l3tcp(self):
+        data = bytes.fromhex(('ffffffffffff' 
+                            + 'ffffffffffff' 
+                            + '0800'
+                            + 'ffffffff'
+                            + 'ffffffff'
+                            + 'ff06ffff'
+                            + 'ffffffff'
+                            + 'ffffffff'))
+        self.assertEqual(get_packet_from_bytes(data)['l3'],
+                         packet.L3TCP)
+        data = bytes.fromhex(('ffffffffffff' 
+                            + 'ffffffffffff' 
+                            + '0800'
+                            + 'ffffffff'
+                            + 'ffffffff'
+                            + 'ff11ffff'
+                            + 'ffffffff'
+                            + 'ffffffff'))
+        self.assertNotEqual(get_packet_from_bytes(data)['l3'], 
+                            packet.L3TCP)
+
+    def test_l3udp(self):
+        data = bytes.fromhex(('ffffffffffff' 
+                            + 'ffffffffffff' 
+                            + '0800'
+                            + 'ffffffff'
+                            + 'ffffffff'
+                            + 'ff11ffff'
+                            + 'ffffffff'
+                            + 'ffffffff'))
+        self.assertEqual(get_packet_from_bytes(data)['l3'],
+                         packet.L3UDP)
+        data = bytes.fromhex(('ffffffffffff' 
+                            + 'ffffffffffff' 
+                            + '0800'
+                            + 'ffffffff'
+                            + 'ffffffff'
+                            + 'ff06ffff'
+                            + 'ffffffff'
+                            + 'ffffffff'))
+        self.assertNotEqual(get_packet_from_bytes(data)['l3'], 
+                            packet.L3UDP)
