21513d9c303cb821a6cc44d32c6ac4ae2c489a0b
Kihyun Choi <ghlove0317@naver.com>
2018-01-24 14:38:10 -0500

    Refactoring done

--- vis/csvTest/csvReader.js
+++ vis/csvTest/csvReader.js
@@ -1,5 +1,8 @@
-var nodes = null;
-var edges = null;
+/**
+ * Created by jinhyeok on 2018. 1. 22..
+ */
+var nodes = [];
+var edges = [];
 var network = null;
 
 var LENGTH_MAIN = 1000,
@@ -55,78 +58,62 @@ var options = {
       }
     }
   };
-
-function readCSV() {
+// get edge
+function readEdgeCSV() {
     $.ajax({
         type:'GET',
-        url: 'save_1.csv',
+        url: 'edge.csv',
         dataType: 'text',
-      }).done(parseCSV);
+      }).done(add_edge);
 }
-/**
- * CSV파일을 parsing하여 node와 edge를 push하는 함수입니다.
- * @author ryan
- * @param {*} data 
- */
-function parseCSV(data) {
-    nodes = []; // Create a data table with nodes.
-    edges = []; // Create a data table with links.
-    var tempSrcIP = [],
-        tempDestIP = [];
-    var duplicateSrcFromSrc = false,
-        duplicateSrcFromDes = false,
-        duplicateDesFromSrc = false,
-        duplicateDesFromDes = false;
+// get node
+function readNodeCSV() {
+    $.ajax({
+        type:'GET',
+        url: 'node.csv',
+         async: false,
+        dataType: 'text',
+      }).done(add_node);
+}
+
+function add_node(data){
     var reg= /\r?\n|\r/;
     var csv = data.split(reg);
-    
-    // Draw nodes
+    // Push all the nodes.
+    for(var row = 1; row < csv.length; row++){
+        var parsed = csv[row].split(',');
+        var srcIp = parsed[0];
+        var weight = parsed[1];
+        console.log(srcIp);
+        nodes.push({id: srcIp, label: srcIp, group: 'internet', value: weight});
+    }
+}
+
+/**
+ * Append nodes and edges after parsing the CSV files.
+ * @author ryan
+ * @param {*} data
+ */
+function add_edge(data) {
+    this.readNodeCSV() // Create nodes
+    var reg= /\r?\n|\r/;
+    var csv = data.split(reg);
+
+    // Create edges
     for(var row = 1; row < csv.length; row++) {
         var parsed = csv[row].split(',');
-        var sip = parsed[1];
-        var dip = parsed[3];
-        // Check duplicate IP address
-        for(var i = 0; i < tempSrcIP.length; i++) {
-          if(sip === tempSrcIP[i]) {
-            duplicateSrcFromSrc = true;
-            //break;
-          }
-          if(sip === tempDestIP[i]) {
-            duplicateSrcFromDes = true;
-            //break;
-          }
-          if(dip === tempSrcIP[i]) {
-            duplicateDesFromSrc = true;
-            //break;
-          }
-          if(dip === tempDestIP[i]) {
-            duplicateDesFromDes = true;
-            //break;
-          }
-        }
-        tempSrcIP.push(sip);
-        tempDestIP.push(dip);
-        
-        if(!duplicateSrcFromSrc && !duplicateSrcFromDes) {
-          nodes.push({id: sip, label: sip, group: 'internet', value: 20});
-        }
-        if(!duplicateDesFromSrc && !duplicateDesFromDes) {
-          if(sip !== dip) {
-            nodes.push({id: dip, label: dip, group: 'internet', value: 20});
-          }
-        }
-        edges.push({from: sip, to: dip, length: LENGTH_SUB, color: GRAY, 
-                    fontColor: GRAY, width: WIDTH_SCALE});
-        duplicateSrcFromSrc = false;
-        duplicateSrcFromDes = false;
-        duplicateDesFromSrc = false;
-        duplicateDesFromDes = false;
+        var sip = parsed[0];
+        var dip = parsed[1];
+        var wid = parsed[2];
+        edges.push({from: sip, to: dip, length: LENGTH_SUB, color: GRAY,
+                    fontColor: GRAY, width: wid});
     }
     var container = document.getElementById('mynetwork');
     var data = {
       nodes: nodes,
       edges: edges
     };
+    // Pushes all created data to Chart Library
     network = new vis.Network(container, data, options);
     network.on("stabilizationIterationsDone", function () {
       network.setOptions({
