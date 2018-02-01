4e1c4cb24040a8f485df9c8e041d3eedd2793ddc
Kihyun Choi <ghlove0317@naver.com>
2018-02-01 15:02:19 -0500

    add_line() method added

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -3,8 +3,8 @@ var targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z 
 // svg path for plane icon
 var planeSVG = "M19.671,8.11l-2.777,2.777l-3.837-0.861c0.362-0.505,0.916-1.683,0.464-2.135c-0.518-0.517-1.979,0.278-2.305,0.604l-0.913,0.913L7.614,8.804l-2.021,2.021l2.232,1.061l-0.082,0.082l1.701,1.701l0.688-0.687l3.164,1.504L9.571,18.21H6.413l-1.137,1.138l3.6,0.948l1.83,1.83l0.947,3.598l1.137-1.137V21.43l3.725-3.725l1.504,3.164l-0.687,0.687l1.702,1.701l0.081-0.081l1.062,2.231l2.02-2.02l-0.604-2.689l0.912-0.912c0.326-0.326,1.121-1.789,0.604-2.306c-0.452-0.452-1.63,0.101-2.135,0.464l-0.861-3.838l2.777-2.777c0.947-0.947,3.599-4.862,2.62-5.839C24.533,4.512,20.618,7.163,19.671,8.11z";
 
-// parameter required: latitude, longitude, country name
-function add_country(){
+// PARAMETER REQUIRED: latitude, longitude, country name
+function add_country() {
   var city = new AmCharts.MapImage();
   city.title = "Vienna";
   city.latitude = 48.2092;
@@ -13,6 +13,17 @@ function add_country(){
   city.chart = map;
   map.dataProvider.images.push(city);
   city.validate();
+}
+
+// PARAMETER REQUIRED: src[lattitude,longitude], dst[lattitude,longitude], weight
+function add_line() {
+  var line = new AmCharts.MapLine();
+  line.latitudes = [ 51.500, 50.4422 ];
+  line.longitudes = [ -0.1262, 30.5367 ];
+  line.chart = map;
+  line.thickness = 10;
+  map.dataProvider.lines.push(line);
+  line.validate();
 }
 
 var map = AmCharts.makeChart( "chartdiv", {
@@ -25,9 +36,6 @@ var map = AmCharts.makeChart( "chartdiv", {
     "zoomLatitude": 49.1712,
 
     "lines": [ {
-      "latitudes": [ 51.5002, 50.4422 ],
-      "longitudes": [ -0.1262, 30.5367 ]
-    }, {
       "latitudes": [ 51.5002, 46.9480 ],
       "longitudes": [ -0.1262, 7.4481 ]
     }, {
