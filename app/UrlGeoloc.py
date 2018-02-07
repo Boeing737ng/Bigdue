190cc29a2fb5101261cba36ce64226f82a51e4bf
Jaesung Chris NA <na-0003@hotmail.com>
2018-02-07 17:45:22 -0500

    geoloc

--- app/UrlGeoloc.py
+++ app/UrlGeoloc.py
@@ -1,7 +1,7 @@
 import json
 from urllib.request import urlopen
 import geoip2.database
-from math import sin, cos, sqrt, atan2, radians
+
 
 class urlGeoloc:
 
@@ -19,29 +19,7 @@ class urlGeoloc:
             
         return [response.location.latitude, response.location.longitude, response.country.iso_code]
 
-    def calculate_distance_btw_two_geoloc(self, src_ip_geoloc, dst_ip_geoloc):
 
-        # approximate radius of earth in km
-        R = 6373.0
-
-        src_latitude = radians(src_ip_geoloc[0])
-        src_longitude = radians(src_ip_geoloc[1])
-        dst_latitude = radians(dst_ip_geoloc[0])
-        dst_longitude = radians(dst_ip_geoloc[1])
-
-        dlon = dst_longitude - src_longitude
-        dlat = dst_latitude - src_latitude
-
-        a = sin(dlat / 2) ** 2 + cos(src_latitude) * cos(dst_latitude) * sin(dlon / 2) ** 2
-        c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-        distance = R * c
-
-        print("Result:", distance)
-        print("Should be:", 278.546, "km")
-        return
-
-        
     # def get_url_geoloc(self, ipaddress):
     #     url_geoloc = urlopen(self.response_url+ipaddress).read().decode('utf-8')
     #     response_json = json.loads(url_geoloc)
