dd9bebb9a4ec83149b90808d682e7fdd24a31897
seong954t <seomtae@naver.com>
2018-02-08 11:21:50 -0500

    add public ipaddress function

--- app/bigdue_app/UrlGeoloc.py
+++ app/bigdue_app/UrlGeoloc.py
@@ -6,19 +6,27 @@ from math import sin, cos, sqrt, atan2, radians
 class urlGeoloc:
 
     def __init__(self):
-        self.response_url = "http://freegeoip.net/json/"
         self.reader = geoip2.database.Reader('geolite/GeoLite2-City.mmdb')
-        checkip = urlopen('http://checkip.dyndns.org').read()
-        self.public_ip = str(checkip).split('IP Address: ')[1].split('<')[0]
+        self.set_public_ipaddress()
 
     def get_url_geoloc(self, ipaddress):
         try:
             response = self.reader.city(ipaddress)
         except:
-            response = self.reader.city(self.public_ip)
+            response = self.reader.city(self.get_public_ipaddress())
             
         return [response.location.latitude, response.location.longitude, response.country.name, response.subdivisions.most_specific.name, response.city.name]
 
+    def set_public_ipaddress(self, ipaddress=None):
+        if ipaddress is None:
+            checkip = urlopen('http://checkip.dyndns.org').read()
+            ipaddress = str(checkip).split('IP Address: ')[1].split('<')[0]
+            
+        self.public_ip = ipaddress
+
+    def get_public_ipaddress(self):
+        return self.public_ip
+    
     def calculate_distance_btw_two_geoloc(self, src_ip_geoloc, dst_ip_geoloc):
 
         # approximate radius of earth in km
