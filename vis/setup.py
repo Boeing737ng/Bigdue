61c566b65b1ef80d0bfe0dd0e81ff50640ed4540
Kimjinhyeok <persona5173@gmail.com>
2018-02-09 11:23:04 -0500

    modified setup.py

--- vis/setup.py
+++ vis/setup.py
@@ -3,4 +3,17 @@ from setuptools import setup, find_packages
 setup(name='packetvisulizaion',
       version='1.2.0',
       description='Packet Visualization',
+      url='https://github.com/Boeing737ng/Bigdue',
+      license='GNU',
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development',
+            'License :: OSI Approved :: GNU License',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+      ],
+      packages=find_packages(exclude=['docs', 'tests']),
       )
