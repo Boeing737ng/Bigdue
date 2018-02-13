4bfdcd4e75773f63c2643fb261533edc380abc96
Kihyun Choi <ghlove0317@naver.com>
2018-02-13 14:11:09 -0500

    Unused comments removed

--- static/js/main.js
+++ static/js/main.js
@@ -4,14 +4,10 @@ function convertTimestamp(){
     for(var i = 0 ; i < timestamp.length; i++){
         var integer_time = parseInt(timestamp[i]);
         var date = new Date(integer_time*1000);
-        // Hours part from the timestamp
         var hours = date.getHours();
-        // Minutes part from the timestamp
         var minutes = "0" + date.getMinutes();
-        // Seconds part from the timestamp
         var seconds = "0" + date.getSeconds();
-
-        // Will display time in 10:30:23 format
+        // Display time in 10:30:23 format
         var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
         console.log(timestamp[i] + " : " + formattedTime);
     }
