8e40ea94b98609b1f999cab8fbf2f0bee764e49c
Kihyun Choi <ghlove0317@naver.com>
2018-02-14 15:58:32 -0500

    Typo fixed. Changed Calulate to Calculate

--- app/makecsv/Write_distance.py
+++ app/makecsv/Write_distance.py
@@ -4,9 +4,9 @@ try:
 except ImportError:
     from app.bigdue_app import UrlGeoloc
 try:
-    import Calulate_distance
+    import Calculate_distance
 except ImportError:
-    from app.bigdue_app import Calulate_distance
+    from app.bigdue_app import Calculate_distance
 import os
 # import Ping
 
@@ -14,7 +14,7 @@ class Write_distance:
     
     def __init__(self):
         self.urlGeoloc = UrlGeoloc.UrlGeoloc()
-        self.calculate_distance = Calulate_distance.Calculate_distance()
+        self.calculate_distance = Calculate_distance.Calculate_distance()
         self.remove_distance_csv_file_list()
         # self.ping = Ping.Ping()
         pass
