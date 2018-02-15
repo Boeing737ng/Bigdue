501ecc311da0ea21138217506035d3ff8cca1ad2
seong954t <seomtae@naver.com>
2018-02-15 15:49:42 -0500

    add print reading packet comment

--- app/makecsv/Read_packet.py
+++ app/makecsv/Read_packet.py
@@ -13,6 +13,7 @@ class Read_packet:
         self.get_csv_file_list()
 
     def read_packet(self, start, end):
+        print('packets reading start')
             for csvs in self.csvlist[int(start):int(end)]:
                 with open(self.dirpath+csvs, encoding = 'utf-8') as csvfile:
                     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -26,7 +27,7 @@ class Read_packet:
                             'dst_port' : packets[4],
                             'packet_size' : packets[5]})
 
-            print("read "+str(len(self.packet_list))+"packets")
+            print('read '+str(len(self.packet_list))+'packets')
             return self.packet_list
 
     def create_folder(self, file_name=None):                                                                                                            
