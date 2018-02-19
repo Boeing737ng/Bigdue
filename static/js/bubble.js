aad18b33b61be1067de32625cdcc8aac44138c05
Kihyun Choi <ghlove0317@naver.com>
2018-02-19 01:51:27 -0500

    d3 undefined error resolved

--- static/js/bubble.js
+++ static/js/bubble.js
@@ -1,20 +1,21 @@
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
-
-var format = d3.format(",d");
-
-var color = d3.scaleOrdinal(d3.schemeCategory20c);
-
-var file_root = 'static/data/graph/';
-var pack = d3.pack()
-    .size([width, height])
-    .padding(1.5);
-
 
 $(document).ready(function() {
   if (window.location.pathname == '/bubble') {
     hideLoading();
+
+    var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+    var format = d3.format(",d");
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20c);
+
+    var file_root = 'static/data/graph/';
+    var pack = d3.pack()
+        .size([width, height])
+        .padding(1.5);
+
     document.body.style.overflow = 'auto';
     var timeStamp = localStorage.getItem('current_timestamp');
     console.log("bubble:" + timeStamp);
