8641d3f4ce4dbd0653c23563273e7a1305fa9c48
munhyunsu <munhyunsu@gmail.com>
2018-02-20 14:22:05 -0500

    linux capture

---
+++ tcpdump/linux_rotate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${SRCDIR}
+
+
+trap ctrl_c INT
+
+function ctrl_c() {
+    chmod -R 777 data
+}
+
+mkdir -p data
+tcpdump -i any -C 1024 -w data/`date -u +%s_%N`.pcap
